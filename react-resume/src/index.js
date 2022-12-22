2de6eed68e48c68df0428dec0c4a551943ab1112
kateewrightt <wrighk@outlook.com>
2022-12-22 20:49:30 +1000

    setup gh-pages

--- react-resume/src/index.js
+++ react-resume/src/index.js
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import Skills from "./pages/Skills";
 import Ambitions from "./pages/Ambitions";
@@ -8,13 +8,13 @@ import Experience from "./pages/Experience";
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename="/">
       <Routes>
-        <Route path="/" element={<Skills />} />
-        <Route path="/ambitions" element={<Ambitions />} />
-        <Route path="/experience" element={<Experience />} />
+        <Route path="react-resume/" element={<Skills />} />
+        <Route path="react-resume/#/ambitions" element={<Ambitions />} />
+        <Route path="react-resume/#/experience" element={<Experience />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
   rootElement
 );
